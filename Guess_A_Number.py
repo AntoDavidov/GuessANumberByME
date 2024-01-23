5c31bb6d9e3a2ce56d282c84e8cbadde137b4052
CrustCrafters <youremail@example.com>
2024-01-23 18:14:25 +0100

    Added a stop option during the game

--- Guess_A_Number.py
+++ Guess_A_Number.py
@@ -1,18 +1,24 @@
 import random
+
 while True:
-    number = (random.randrange(0, 101))
+    number = random.randrange(1, 101)
     guess = 0
     best_score = float('inf')
+
     while True:
-        number_guess = int(input("Take a guess:"))
+        number_guess = input("Take a guess or write 'stop' to end the game:")
+        if number_guess.lower() == "stop":
+            break
         try:
-            number_guess = int(number_guess)
-            if number_guess not in range(0, 101):
+            number_guess_as_int = int(number_guess)
+
+
+            if number_guess_as_int not in range(1, 101):
                 print("Please enter a number between 1-100")
                 continue
 
             guess += 1
-            if number_guess == number:
+            if number_guess_as_int == number:
                 print(f"You guessed it. It took you {guess} attempts")
                 if guess < best_score:
                     best_score = guess
@@ -21,12 +27,13 @@ while True:
                     print(f"Best record: {best_score} attempts.")
                 break
 
-            elif number_guess < number:
+            elif number_guess_as_int < number:
                 print("Hint: Too low!")
             else:
                 print("Hint: Too high")
         except ValueError:
             print("Enter a valid number")
+
     play_again = input("Do you want to play again? (yes/no):").lower()
     if play_again != "yes":
         break
